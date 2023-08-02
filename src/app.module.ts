7cd5f3c91b48804e3fb4bec7a7bc6bbabf734c6b
FarruxDEV <farruxbeknozimov@gmail.com>
2023-08-02 11:00:08 +0500

    change on service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { resolve } from 'path';
 import { ClinicModule } from './clinic/clinic.module';
+import { join } from 'path';
+import { ServiceModule } from './service/service.module';
+import { Service } from './service/models/service.model';
+import { Admin } from './admin/models/admin.model';
+import { Client } from './client/models/client.model';
+import { Clinic } from './clinic/models/clinic.model';
+import { Diagnosis } from './diagnosis/models/diagnosis.model';
+import { Doctor } from './doctor/models/doctor.model';
+import { Queue } from './queue/models/queue.model';
+import { AdminModule } from './admin/admin.module';
+import { ClientModule } from './client/client.module';
+import { DiagnosisModule } from './diagnosis/diagnosis.module';
+import { DoctorModule } from './doctor/doctor.module';
+import { QueueModule } from './queue/queue.module';
 
 @Module({
   imports: [
@@ -12,7 +25,8 @@ import { ClinicModule } from './clinic/clinic.module';
       isGlobal: true,
     }),
     ServeStaticModule.forRoot({
-      rootPath: resolve(__dirname, 'static'),
+      rootPath: join(__dirname, '..', 'swagger-static'),
+      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
     }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
@@ -21,6 +35,7 @@ import { ClinicModule } from './clinic/clinic.module';
       username: process.env.PG_USER,
       password: String(process.env.PG_PASSWORD),
       database: process.env.PG_DB,
+      models: [Admin, Service, Client, Clinic, Diagnosis, Doctor, Queue],
       autoLoadModels: true,
       logging: false,
       ssl: true,
@@ -30,9 +45,14 @@ import { ClinicModule } from './clinic/clinic.module';
           rejectUnauthorized: false,
         },
       },
-      models: [],
     }),
+    AdminModule,
+    ServiceModule,
+    ClientModule,
     ClinicModule,
+    DiagnosisModule,
+    DoctorModule,
+    QueueModule,
   ],
   controllers: [],
   providers: [],
